a5cedb981b356a08689176fd8ce8e0a24e16d9c4
AbdullahErzin <abdullah.erzin@gensys.com.tr>
2022-01-19 17:57:48 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace List_n_Thread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.textBox1);
+            this.groupControl1.Controls.Add(this.simpleButton1);
+            this.groupControl1.Location = new System.Drawing.Point(120, 29);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(360, 383);
+            this.groupControl1.TabIndex = 0;
+            this.groupControl1.Text = "groupControl1";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(148, 340);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "simpleButton1";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 54);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(151, 257);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.groupControl1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private System.Windows.Forms.TextBox textBox1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+
+
+namespace List_n_Thread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int i = 0;
+        int add = 777;
+        List<int> arrayList = new List<int>();
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            createList(arrayList, 20);
+
+            
+            for (int Index = 0; Index < arrayList.Count; Index++)
+            {
+                
+
+                Parallel.Invoke(
+                   ()=> basicTextWrite(arrayList, Index),
+                   () => writeFunc(Index),
+                   () => deleteFunc(Index)
+
+                   );
+                //textBox1.Text += arrayList[Index].ToString() + "\r\n";
+
+            }
+
+
+        }
+        private void writeFunc(int Index)
+        {                        
+            
+            if (Index % 3==0 && Index!=0)
+            {
+                arrayList.Insert(Index, add);
+            }
+            
+        }
+        private void deleteFunc(int Index)
+        {
+            
+            if (Index % 6 == 0 && Index != 0)
+            {
+                arrayList.RemoveAt(Index);
+            }
+            
+        }
+        private void basicTextWrite(List<int> list, int Index)
+        {   
+            textBox1.Text += list[Index].ToString() + "\r\n";
+        }
+        private void createList(List <int> list,int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                list.Add(i);
+            }
+        }
+        
+
+
+
+    }
+}
